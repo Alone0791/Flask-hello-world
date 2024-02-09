6940e5b9ccf3bb2255dd72b34365cd015a1a7512
آفتاب عالم ٭ <103352133+Alone0791@users.noreply.github.com>
2024-02-09 17:29:21 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,18 +1,1 @@
-from flask import Flask, request
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    file = open("hi", "r")
-    return f"<pre><code>{file.read()}<code><pro>"
-
-@app.route('/pro', methods=['GET', 'POST'])
-def paste():
-    if request.method == 'POST':
-        file = open("hi", "w")
-        file.write(request.data.decode("utf-8"))
-        return "Done"
-
-if __name__ == '__main__':
-    app.run(debug=True)
+<p>hello world</p>
